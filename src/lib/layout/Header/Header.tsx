0180be9396993f4ffa93b984b51a64308acff936
hugrid2712 <2712@trustless.computer>
2023-03-28 16:06:16 +0700

    feat: update header footer

--- src/lib/layout/Header/Header.tsx
+++ src/lib/layout/Header/Header.tsx
@@ -1,5 +1,8 @@
 import { Wrapper } from "./Header.styled";
 // import IcLogo from "./icons/logo.svg";
+import IcDiscord from "./icons/ic_discord.svg";
+// import IcGithub from "./icons/ic-github.svg";
+import IcTwitter from "./icons/ic_twitter.svg";
 
 const Header = ({ height }: { height: number }) => {
   return (
@@ -8,11 +11,19 @@ const Header = ({ height }: { height: number }) => {
         <img alt="logo" src={IcLogo} />
       </a> */}
       <div />
-      <a href="https://explorer.trustless.computer/">
-        <div>
-          <p className="networkText">Explorer</p>
-        </div>
-      </a>
+      <div className="row">
+        <a href="https://explorer.trustless.computer/">
+          <div>
+            <p className="networkText">Explorer</p>
+          </div>
+        </a>
+        <a className="iconContainer" href="https://discord.gg/tscNGxEw2s">
+          <img alt="icon" className="icon" src={IcDiscord} />
+        </a>
+        <a className="iconContainer" href="https://twitter.com/DappsOnBitcoin">
+          <img alt="icon" className="icon" src={IcTwitter} />
+        </a>
+      </div>
     </Wrapper>
   );
 };
