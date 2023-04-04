4951347a3c5e61f6079c4cd161a321c5f20c6525
hugrid2712 <2712@trustless.computer>
2023-04-04 12:13:30 +0700

    feat: integrate api fauctet

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { RouteObject } from 'react-router-dom';
 import Layout from '@/pages/layout';
 import Home from '@/pages/home';
-import Faucet from '@/pages/faucet';
+// import Faucet from '@/pages/faucet';
 import NotFound from '@/pages/404';
 
 export default [
@@ -11,11 +11,11 @@ export default [
     element: <Layout />,
     children: [{ index: true, element: <Home /> }],
   },
-  {
-    path: '/faucet',
-    element: <Layout />,
-    children: [{ index: true, element: <Faucet /> }],
-  },
+  // {
+  //   path: '/faucet',
+  //   element: <Layout />,
+  //   children: [{ index: true, element: <Faucet /> }],
+  // },
   {
     path: '/*',
     element: <NotFound />,
