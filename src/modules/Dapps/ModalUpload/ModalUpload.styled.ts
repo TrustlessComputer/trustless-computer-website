369f04fd98607e9f767f9d9fff87dda51ab6b86f
Duc Anh Nguyen <ducanh@myconstant.com>
2023-04-04 15:10:52 +0700

    feat: wip bfs

--- src/modules/Dapps/ModalUpload/ModalUpload.styled.ts
+++ src/modules/Dapps/ModalUpload/ModalUpload.styled.ts
@@ -1,6 +1,6 @@
 import px2rem from '@/utils/px2rem';
 import { Modal } from 'react-bootstrap';
-import styled from 'styled-components';
+import styled, { DefaultTheme } from 'styled-components';
 
 export const StyledModalUpload = styled(Modal)`
   &.modal {
@@ -26,5 +26,46 @@ export const StyledModalUpload = styled(Modal)`
 
   .modal-footer {
     border-top: none;
+  }
+
+  /* ======= Custom modal ========== */
+
+  .dropZone {
+    margin-top: ${px2rem(24)};
+
+    width: 100%;
+  }
+
+  .preview-wrapper {
+    img {
+      background-color: ${({ theme }: { theme: DefaultTheme }) => theme.bg5};
+      max-height: ${px2rem(281)};
+      width: 100%;
+      object-fit: contain;
+      margin-left: auto;
+      margin-right: auto;
+      margin-bottom: ${px2rem(12)};
+      cursor: pointer;
+    }
+  }
+
+  .error-text {
+    color: ${({ theme }: { theme: DefaultTheme }) => theme.text6};
+  }
+  .file-upload-name {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .upload-fee {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: ${px2rem(16)};
+
+    p {
+      color: ${({ theme }: { theme: DefaultTheme }) => theme.text7};
+    }
   }
 `;
