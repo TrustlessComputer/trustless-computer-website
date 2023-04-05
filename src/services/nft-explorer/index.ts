e6d67a65b1a485ab9e2cc1b23f3e5a07d413b2bf
hugrid2712 <2712@trustless.computer>
2023-04-05 14:39:20 +0700

    fix: process env

--- src/services/nft-explorer/index.ts
+++ src/services/nft-explorer/index.ts
@@ -1,4 +1,4 @@
-import { API_BASE_URL } from './../../constants/config';
+import { API_BASE_URL } from '@/utils/configs';
 import axios from 'axios';
 import { ICollection } from '@/models/collection';
 
