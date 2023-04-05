bd0b248aff10fbf4f32e4725fc792e8de9f32eb1
benjiro2077 <benjiro2077@generative.xyz>
2023-04-05 15:06:14 +0700

    feat: contract structure

--- src/utils/xverse-wallet-storage.ts
+++ src/utils/xverse-wallet-storage.ts
@@ -1,6 +1,6 @@
 import { XVERSE_WALLET_ADDRESS } from '@/constants/storage-key';
 import { IXverseWalletStorage } from '@/interfaces/xverse-wallet';
-import sessionStorage from '@/utils/session-storage';
+import sessionStorage from '@/utils/sessionstorage';
 
 export const setXverseWallet = (payload: IXverseWalletStorage): void => {
   return sessionStorage.set(XVERSE_WALLET_ADDRESS, JSON.stringify(payload));
