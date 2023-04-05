4e165d6d472d1ea443c926cd6707c3ef412a30eb
archaon <archaon@generative.xyz>
2023-04-05 13:47:19 +0700

    feat: wip artifacts

--- src/services/nft-explorer/index.ts
+++ src/services/nft-explorer/index.ts
@@ -1,11 +1,15 @@
 import { API_BASE_URL } from './../../constants/config';
 import axios from 'axios';
 
-const API_PATH = '/bfs-service';
+const API_PATH = '/collections';
 
 export const fetcher = (url: string) => axios.get(API_BASE_URL + url).then(res => res.data);
 
-export const getCollections = () => fetcher(`/nft-explorer/collections`);
+export const getCollections = () => fetcher(API_PATH);
+
+// TODO: Add iterface for response
+export const getCollectionDetail = ({ contractAddress, tokenID }: { contractAddress: string; tokenID: string }): any =>
+  fetcher(`${API_PATH}/${contractAddress}/nfts/${tokenID}`);
 
 // export const fetchBFSFiles = ({ address }: { address: string }): any => {
 //   return fetcher(`${API_PATH}/files/${address}`);
