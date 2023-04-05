6de5318cf3031a01b06faf35b16f72e98a506544
archaon <archaon@generative.xyz>
2023-04-05 11:40:15 +0700

    feat: add swr for data fetching

---
+++ src/services/bfs/index.ts
@@ -0,0 +1,14 @@
+import { API_BASE_URL } from './../../constants/config';
+import axios from 'axios';
+
+const API_PATH = '/bfs-service';
+
+export const fetcher = (url: string) => axios.get(API_BASE_URL + url).then(res => res.data);
+
+export const getCollections = () => fetcher(`/nft-explorer/collections`);
+
+export const fetchBFSFiles = ({ address }: { address: string }): any => {
+  console.log('fetchBFSFiles', address);
+
+  return fetcher(`${API_PATH}/files/${address}`);
+};
--- src/services/client.ts
+++ src/services/client.ts
@@ -1,10 +1,11 @@
+import { API_BASE_URL } from './../constants/config';
 import { AxiosInstance } from 'axios';
 import { createAxiosInstance } from '@/services';
 
 class Client {
   http: AxiosInstance;
   constructor() {
-    this.http = createAxiosInstance({ baseURL: '' });
+    this.http = createAxiosInstance({ baseURL: API_BASE_URL });
   }
 }
 
