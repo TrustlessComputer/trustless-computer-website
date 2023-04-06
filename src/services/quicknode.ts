8364228922c01ad8fbb05ae412f005a8689c1e09
benjiro2077 <benjiro2077@generative.xyz>
2023-04-06 16:26:24 +0700

    feat: btc balance

--- src/services/quicknode.ts
+++ src/services/quicknode.ts
@@ -2,10 +2,10 @@ import { apiClient } from '.';
 
 const API_PATH = '/quicknode';
 
-const getBtcBalance = async (btcWalletAddress: string): Promise<number> => {
+export const getBtcBalance = async (btcWalletAddress: string): Promise<number> => {
   try {
     const res = await apiClient.get(`${API_PATH}/address/${btcWalletAddress}/balance`);
-    return 0;
+    return Object(res)[0].value;
   } catch (err: unknown) {
     console.log(err);
     return 0;
