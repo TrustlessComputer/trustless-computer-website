7e51b4ad5a43a7e93e293db3e94ec52e122548cf
hugrid2712 <2712@trustless.computer>
2023-04-06 17:43:28 +0700

    feat: create modal

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -7,11 +7,12 @@ export type ButtonProps = {
   onClick?: () => void;
   className?: string;
   props?: HTMLButtonElement;
+  type?: 'submit' | 'reset' | 'button' | undefined;
 };
 
-const Button = ({ bg = 'bg1', className, onClick, children, ...props }: PropsWithChildren<ButtonProps>) => {
+const Button = ({ type, bg = 'bg1', className, onClick, children, ...props }: PropsWithChildren<ButtonProps>) => {
   return (
-    <StyledButton bg={bg} className={className} onClick={onClick} {...props}>
+    <StyledButton type={type} bg={bg} className={className} onClick={onClick} {...props}>
       {children}
     </StyledButton>
   );
--- src/modules/Dapps/Collections/Collections.tsx
+++ src/modules/Dapps/Collections/Collections.tsx
@@ -1,59 +1,15 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import WrapImage from '@/components/WrapImage';
-import { ICollection } from '@/models/collection';
-import { debounce } from 'lodash';
-import React, { useMemo } from 'react';
-import { useEffect, useState } from 'react';
-import InfiniteScroll from 'react-infinite-scroll-component';
-import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
-
-import { Container } from './Collections.styled';
-import { getCollections } from '@/services/nft-explorer';
-import { shortenAddress } from '@/utils';
-import Spinner from 'react-bootstrap/Spinner';
-import { ARTIFACT_CONTRACT } from '@/configs';
-import Text from '@/components/Text';
-import Button from '@/components/Button';
 import IcBitcoinCloud from '@/assets/icons/ic-bitcoin-721.svg';
+import Button from '@/components/Button';
+import Text from '@/components/Text';
+import React, { useState } from 'react';
 import { UploadFileContainer } from '../Dapps.styled';
-
-const LIMIT_PAGE = 32;
+import { Container } from './Collections.styled';
+import List from './List';
+import ModalCreate from './ModalCreate';
 
 const Collections = () => {
-  const [isFetching, setIsFetching] = useState(false);
-  const [collections, setCollections] = useState<ICollection[]>([]);
-
-  useEffect(() => {
-    fetchCollections();
-  }, []);
-
-  const fetchCollections = async (page = 1, isFetchMore = false) => {
-    try {
-      setIsFetching(true);
-      const data = await getCollections(page, LIMIT_PAGE);
-      if (isFetchMore) {
-        setCollections(prev => [...prev, ...data]);
-      } else {
-        setCollections(data);
-      }
-    } catch (error) {
-    } finally {
-      setIsFetching(false);
-    }
-  };
-
-  const onLoadMoreCollections = () => {
-    if (isFetching || collections.length % LIMIT_PAGE !== 0) return;
-    const page = Math.floor(collections.length / LIMIT_PAGE) + 1;
-    fetchCollections(page, true);
-  };
-
-  const debounceLoadMore = debounce(onLoadMoreCollections, 300);
-
-  const showCollections = useMemo(
-    () => collections.filter(item => item.totalItems > 0 && item.contract !== ARTIFACT_CONTRACT),
-    [collections],
-  );
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <Container>
@@ -69,61 +25,15 @@ const Collections = () => {
           </div>
         </div>
         <div className="upload_right">
-          <Button bg={'white'}>
+          <Button bg={'white'} onClick={() => setShowModal(true)}>
             <Text size="medium" color="bg1" className="button-text" fontWeight="medium">
               Create BRC-721
             </Text>
           </Button>
         </div>
       </UploadFileContainer>
-      <div className="content">
-        <div>
-          <InfiniteScroll
-            className="list"
-            dataLength={showCollections.length}
-            hasMore={true}
-            loader={
-              isFetching && (
-                <div className="loading">
-                  <Spinner animation="border" variant="primary" />
-                </div>
-              )
-            }
-            next={debounceLoadMore}
-          >
-            <ResponsiveMasonry
-              columnsCountBreakPoints={{
-                350: 1,
-                750: 2,
-                900: 3,
-                1240: 4,
-                2500: 5,
-                3000: 5,
-              }}
-            >
-              <Masonry gutter="24px">
-                {showCollections.length > 0 &&
-                  showCollections.map((item, index) => {
-                    return (
-                      <a key={index.toString()} className="card" href={`/collection?contract=${item.contract}`}>
-                        <div className="card-content">
-                          <div className="card-image">
-                            <WrapImage alt={`thumb-${index.toString()}`} className="image" src={item.thumbnail} />
-                          </div>
-                          <div className="card-info">
-                            <p className="card-title">{item.name || shortenAddress(item.contract, 6)}</p>
-                            <p className="card-subTitle">{shortenAddress(item.creator, 4)}</p>
-                            <p className="card-index">Collection #{index + 1}</p>
-                          </div>
-                        </div>
-                      </a>
-                    );
-                  })}
-              </Masonry>
-            </ResponsiveMasonry>
-          </InfiniteScroll>
-        </div>
-      </div>
+      <List />
+      <ModalCreate show={showModal} handleClose={() => setShowModal(false)} />
     </Container>
   );
 };
