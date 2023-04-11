db49c5feea1b0b7bd352c1a9f9d6cbf4f58d0d92
archaon <archaon@generative.xyz>
2023-04-11 16:42:30 +0700

    feat: update nfts tab

--- src/utils/swr.ts
+++ src/utils/swr.ts
@@ -18,6 +18,7 @@ export const getApiKey = (fetcher: BareFetcher, params?: string | string[] | Rec
 };
 
 export const swrFetcher = async (url: string, options: any) => {
+  console.log('🚀 ~ swrFetcher ~ url:', url);
   const { method, data, ...rest } = options;
 
   try {
