26442cb9e53d19f79570fa23a225e89555063a5b
hugrid2712 <2712@trustless.computer>
2023-04-12 16:13:12 +0700

    feat: ui edit collection

--- src/pages/collection/ModalEdit/index.tsx
+++ src/pages/collection/ModalEdit/index.tsx
@@ -1,10 +1,17 @@
 import Button from '@/components/Button';
 import Text from '@/components/Text';
 import { ICollection } from '@/interfaces/api/collection';
-import { Title, WrapInput } from '@/modules/Dapps/Collections/ModalCreate/ModalCreate.styled';
 import { Formik } from 'formik';
 import { Modal } from 'react-bootstrap';
-import { StyledModalUpload } from './ModalMint.styled';
+import { StyledModalUpload, Title, WrapInput } from './ModalMint.styled';
+import IconSVG from '@/components/IconSVG';
+import IcCloseModal from '@/assets/icons/ic-close.svg';
+import { useState, useEffect } from 'react';
+import { MINT_TOOL_MAX_FILE_SIZE } from '@/constants/config';
+import { FileUploader } from 'react-drag-drop-files';
+import DefaultUploadImage from '@/assets/img/default-upload-img.png';
+import MediaPreview from '@/components/ThumbnailPreview/MediaPreview';
+import IcUpload from '@/assets/icons/ic_upload_image.svg';
 
 type Props = {
   collection: ICollection;
@@ -19,6 +26,33 @@ interface IFormValue {
 
 const ModalEdit = (props: Props) => {
   const { show = false, handleClose, collection } = props;
+
+  const [file, setFile] = useState<File | null>(null);
+  const [preview, setPreview] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  const [isProcessing, setIsProcessing] = useState<boolean>(false);
+
+  const onChangeFile = (file: File): void => {
+    setFile(file);
+    setError('');
+  };
+
+  const onSizeError = (): void => {
+    setError(`File size error, maximum file size is ${MINT_TOOL_MAX_FILE_SIZE * 1000}KB.`);
+    setPreview(null);
+  };
+
+  useEffect(() => {
+    if (file) {
+      const fileSizeInKb = file.size / 1024;
+      if (fileSizeInKb > MINT_TOOL_MAX_FILE_SIZE * 1000) {
+        onSizeError();
+      } else {
+        setPreview(URL.createObjectURL(file));
+      }
+    }
+  }, [file]);
 
   const validateForm = (values: IFormValue): Record<string, string> => {
     const errors: Record<string, string> = {};
@@ -35,13 +69,19 @@ const ModalEdit = (props: Props) => {
 
   const handleSubmit = async (values: IFormValue): Promise<void> => {
     const { name, description } = values;
+    try {
+      setIsProcessing(true);
+    } catch (error) {
+    } finally {
+      setIsProcessing(false);
+    }
   };
 
   return (
     <StyledModalUpload show={show} onHide={handleClose} centered>
-      {/* <Modal.Header>
+      <Modal.Header>
         <IconSVG className="cursor-pointer" onClick={handleClose} src={IcCloseModal} maxWidth={'22px'} />
-      </Modal.Header> */}
+      </Modal.Header>
       <Modal.Body>
         <Title className="font-medium">Edit project: {collection.name}</Title>
         <Formik
@@ -56,7 +96,7 @@ const ModalEdit = (props: Props) => {
           {({ values, errors, touched, handleChange, handleBlur, handleSubmit }) => (
             <form onSubmit={handleSubmit}>
               <WrapInput>
-                <p className="title-input">Enter name</p>
+                <p className="title-input">Collection name</p>
                 <input
                   id="name"
                   type="text"
@@ -71,32 +111,53 @@ const ModalEdit = (props: Props) => {
               </WrapInput>
 
               <WrapInput>
-                <p className="title-input">Enter description</p>
+                <p className="title-input">Description</p>
                 <input
-                  id="symbol"
+                  id="description"
                   type="text"
-                  name="symbol"
+                  name="description"
                   onChange={handleChange}
                   onBlur={handleBlur}
                   value={values.description}
                   className="input"
-                  placeholder={`Enter collection description`}
+                  placeholder={`Enter description`}
                 />
                 {errors.description && touched.description && <p className="error">{errors.description}</p>}
               </WrapInput>
 
               <div>
-                <Button className="confirm-btn">
-                  <Text size="medium" fontWeight="medium" className="confirm-text">
-                    Cancel
-                  </Text>
-                </Button>
-                <Button className="confirm-btn">
-                  <Text size="medium" fontWeight="medium" className="confirm-text">
-                    Confirm
-                  </Text>
-                </Button>
+                <div className="preview-wrapper">
+                  {preview ? (
+                    <div className="thumbnail-wrapper">
+                      <MediaPreview previewExt={file?.name?.split('.')?.pop() || ''} previewUrl={preview} />
+                    </div>
+                  ) : (
+                    <img src={collection.thumbnail || DefaultUploadImage} alt="default upload image"></img>
+                  )}
+                </div>
+
+                <FileUploader
+                  handleChange={onChangeFile}
+                  name={'fileUploader'}
+                  maxSize={0.35}
+                  onSizeError={onSizeError}
+                  classes={'dropZone'}
+                  types={['png', 'jpeg', 'jpg', 'webp']}
+                >
+                  <div className="upload-btn">
+                    <IconSVG src={IcUpload} maxWidth={'22px'} />
+                    <p className="upload-text">Upload thumbnail</p>
+                  </div>
+                </FileUploader>
+
+                {error && <p className={'error-text'}>{error}</p>}
               </div>
+
+              <Button className="confirm-btn" disabled={isProcessing}>
+                <Text size="medium" fontWeight="medium" className="confirm-text">
+                  Update
+                </Text>
+              </Button>
             </form>
           )}
         </Formik>
