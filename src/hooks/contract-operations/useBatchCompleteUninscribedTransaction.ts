5c58a5158079385ec12143697e3b047efce44fe3
archaon <archaon@generative.xyz>
2023-04-12 19:18:46 +0700

    feat: wip transactions

---
+++ src/hooks/contract-operations/useBatchCompleteUninscribedTransaction.ts
@@ -0,0 +1,98 @@
+import { SupportedChainId } from '@/constants/chains';
+import { WalletContext } from '@/contexts/wallet-context';
+import { switchChain } from '@/utils';
+import { useWeb3React } from '@web3-react/core';
+import { useContext, useState } from 'react';
+import useBitcoin from '../useBitcoin';
+import { useSelector } from 'react-redux';
+import { getUserSelector } from '@/state/user/selector';
+import { AssetsContext } from '@/contexts/assets-context';
+import { updateStatusTransaction } from '@/services/profile';
+import { toast } from 'react-hot-toast';
+
+interface IParams {
+  chainId?: SupportedChainId;
+}
+
+const useBatchCompleteUninscribedTransaction = (args: IParams) => {
+  const { chainId = SupportedChainId.TRUSTLESS_COMPUTER } = args;
+  const { feeRate, getAvailableAssetsCreateTx } = useContext(AssetsContext);
+  const { chainId: walletChainId, connector } = useWeb3React();
+  const { onConnect: onConnectMetamask } = useContext(WalletContext);
+  const user = useSelector(getUserSelector);
+  const { createBatchInscribeTxs, getUnInscribedTransactionDetailByAddress } = useBitcoin();
+  const [transactionConfirmed, setTransactionConfirmed] = useState(false);
+
+  const connectWallet = async () => {
+    try {
+      if (!user?.walletAddress) {
+        return await onConnectMetamask();
+      }
+      return user.walletAddress;
+    } catch (err: unknown) {
+      console.log(err);
+      throw Error('Failed to connect wallet');
+    }
+  };
+
+  const checkAndSwitchChainIfNecessary = async (): Promise<void> => {
+    if (walletChainId !== chainId) {
+      await switchChain(chainId);
+    }
+  };
+
+  const run = async (): Promise<void> => {
+    try {
+      // This function does not handle error
+      // It delegates error to caller
+
+      // Connect Metamask & Xverse
+      const address = await connectWallet();
+      if (!address) {
+        throw Error('Wallet address not found');
+      }
+
+      // Check & switch network if necessary
+      await checkAndSwitchChainIfNecessary();
+      const assets = await getAvailableAssetsCreateTx();
+
+      console.log(assets);
+
+      // Check unInscribed transactions
+      const unInscribedTxDetails = await getUnInscribedTransactionDetailByAddress(address);
+
+      if (unInscribedTxDetails.length === 0) {
+        throw Error('No pending transaction found.');
+      }
+
+      console.log('unInscribedTxDetails', unInscribedTxDetails);
+
+      console.log('feeRatePerByte', feeRate.fastestFee);
+
+      // Make inscribe transaction
+      const res = await createBatchInscribeTxs({
+        tcTxDetails: [...unInscribedTxDetails],
+        feeRatePerByte: feeRate.fastestFee,
+      });
+
+      if (res) {
+        const updateConfirm = await updateStatusTransaction({ txHash: unInscribedTxDetails.map(tx => tx.Hash) });
+        if (updateConfirm && updateConfirm.length > 0) {
+          toast.success('Transaction completed successfully');
+          setTransactionConfirmed(true);
+          // console.log('updateConfirm', updateConfirm);
+        }
+      }
+    } catch (err: any) {
+      console.log(err);
+      throw err;
+    }
+  };
+
+  return {
+    run,
+    transactionConfirmed,
+  };
+};
+
+export default useBatchCompleteUninscribedTransaction;
--- src/services/profile.ts
+++ src/services/profile.ts
@@ -1,6 +1,7 @@
 import { IProfileResponse } from '@/interfaces/api/profile';
 import { apiClient } from '.';
 import { camelCaseKeys } from '@/utils/helpers';
+import { IPagingParams } from '@/interfaces/api/query';
 
 const API_PATH = '/profile';
 
@@ -14,9 +15,9 @@ export const getCurrentProfile = async (): Promise<IProfileResponse> => {
   }
 };
 
-export const updateStatusTransaction = async ({ txHash }: { txHash: string[] }): Promise<IProfileResponse> => {
+export const updateStatusTransaction = async ({ txHash }: { txHash: string[] }): Promise<any> => {
   try {
-    const res = await apiClient.put(`${API_PATH}/histories/confirm`, { txHash });
+    const res = await apiClient.put(`${API_PATH}/histories/confirm`, { tx_hash: txHash });
     return Object(camelCaseKeys(res));
   } catch (err: unknown) {
     console.log(err);
@@ -24,15 +25,40 @@ export const updateStatusTransaction = async ({ txHash }: { txHash: string[] }):
   }
 };
 
-export const createTransactionHistory = async (payload: {
-  dapp_type: string;
-  tx_hash: string;
-}): Promise<IProfileResponse> => {
+export const createTransactionHistory = async (payload: { dapp_type: string; tx_hash: string }): Promise<any> => {
   try {
     const res = await apiClient.post(`${API_PATH}/histories`, payload);
     return Object(camelCaseKeys(res));
   } catch (err: unknown) {
     console.log(err);
     throw Error('Fail to update status transaction');
+  }
+};
+
+export const getTransactionsByWallet = async ({
+  walletAddress,
+  limit = 200,
+  page = 1,
+}: { walletAddress: string } & IPagingParams): Promise<any> => {
+  try {
+    const res = await apiClient.get(`${API_PATH}/wallet/${walletAddress}/histories?limit=${limit}&page=${page}`);
+    return Object(camelCaseKeys(res));
+  } catch (err: unknown) {
+    console.log(err);
+    throw Error('Profile not found');
+  }
+};
+
+export const getCollectionsByItemsOwned = async ({
+  walletAddress,
+  limit,
+  page,
+}: { walletAddress: string } & IPagingParams): Promise<any> => {
+  try {
+    const res = await apiClient.get(`${API_PATH}/wallet/${walletAddress}/collections`);
+    return Object(camelCaseKeys(res));
+  } catch (err: unknown) {
+    console.log(err);
+    throw Error('Profile not found');
   }
 };
