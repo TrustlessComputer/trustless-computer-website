4f9de4d9c573d7b9d3947cba14888c9e5256cbb0
hugrid2712 <2712@trustless.computer>
2023-04-12 11:45:27 +0700

    fix: refactor NFT card

--- src/pages/collection/Collection.styled.ts
+++ src/pages/collection/Collection.styled.ts
@@ -32,73 +32,10 @@ const Container = styled.div`
       justify-content: center;
       margin-top: 32px;
     }
-
-    .card {
-      width: 100%;
-      height: auto;
-      text-decoration: none;
-      --bs-card-bg: none;
-    }
-
-    .card-content {
-      background: #2e2e2e;
-      border: 1px solid transparent;
-      :hover {
-        border: 1px solid #d9d9d9;
-      }
-    }
-
-    .card-image {
-      background: #5b5b5b;
-      .image {
-        min-height: 100px;
-        width: 100%;
-        aspect-ratio: 1 / 1;
-        height: auto;
-        object-fit: cover;
-        padding: 32px;
-      }
-    }
-
-    .card-info {
-      padding: 16px 24px;
-      .card-title {
-        font-style: normal;
-        font-weight: 500;
-        font-size: 20px;
-        line-height: 30px;
-        letter-spacing: -0.01em;
-        color: #ffffff;
-      }
-
-      .card-subTitle {
-        font-style: normal;
-        font-weight: 500;
-        font-size: 18px;
-        line-height: 28px;
-        color: #898989;
-      }
-    }
   }
 
   ${({ theme }: { theme: DefaultTheme }) => theme.deprecated_mediaWidth.deprecated_upToMedium`
     .content {
-
-      .card-image {
-        .image {
-          padding: 24px;
-        }
-      }
-      
-      .card-info {
-        .card-title {
-          
-        }
-  
-        .card-subTitle {
-          
-        }
-      }
 
       .list {
         margin-top: 264px;
