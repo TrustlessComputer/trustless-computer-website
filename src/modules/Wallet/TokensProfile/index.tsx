485696cdf812840e6f6185beacb3b2652f85b540
archaon <archaon@generative.xyz>
2023-04-14 13:30:01 +0700

    feat: show user's tokens owned

--- src/modules/Wallet/TokensProfile/index.tsx
+++ src/modules/Wallet/TokensProfile/index.tsx
@@ -8,58 +8,105 @@ import { useWeb3React } from '@web3-react/core';
 import { Spinner } from 'react-bootstrap';
 import useSWR from 'swr';
 import { StyledTokenProfile } from './TokenProfile.styled';
+import { getTokensWallet } from '@/services/profile';
+import { useEffect, useState } from 'react';
+import { debounce } from 'lodash';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { useSelector } from 'react-redux';
+import { getUserSelector } from '@/state/user/selector';
 
 const EXPLORER_URL = TRUSTLESS_COMPUTER_CHAIN_INFO.explorers[0].url;
 
-const TokensProfile = () => {
-  const { account } = useWeb3React();
+const LIMIT_PAGE = 12;
 
-  const profileWallet = account || '';
+const TokensProfile = () => {
+  const user = useSelector(getUserSelector);
+
+  const profileWallet = user?.walletAddress || '';
+  const [isFetching, setIsFetching] = useState(false);
+  const [tokensList, setTokensList] = useState<any>([]);
 
   const TABLE_HEADINGS = ['Token number', 'Name', 'Symbol', 'Supply', ''];
 
-  const { data, error, isLoading } = useSWR(getApiKey(getTokensByWallet, { key: profileWallet }), () =>
-    getTokensByWallet({ key: profileWallet }),
+  // const { data, error, isLoading } = useSWR(getApiKey(getTokensByWallet, { key: profileWallet }), () =>
+  //   getTokensByWallet({ key: profileWallet }),
+  // );
+
+  const fetchTokensOwned = async (page = 1, isFetchMore = false) => {
+    try {
+      setIsFetching(true);
+      const res = await getTokensWallet({ walletAddress: profileWallet, limit: 12, page: page });
+      if (isFetchMore) {
+        setTokensList((prev: any) => [...prev, ...res]);
+      } else {
+        setTokensList(res);
+      }
+    } catch (err: unknown) {
+      console.log('Failed to fetch tokens owned');
+    }
+  };
+
+  const tokenDatas = tokensList.map(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (token: any, index: number) => {
+      const totalSupply = token?.totalSupply / decimalToExponential(token.decimal);
+      const linkTokenExplorer = `${EXPLORER_URL}/token/${token?.address}`;
+
+      return {
+        id: `token-${token?.address}}`,
+        render: {
+          number: index + 1,
+          name: (
+            <a href={linkTokenExplorer} rel="rel=”noopener noreferrer”" target="_blank">
+              {token?.name || '-'}
+            </a>
+          ),
+
+          symbol: token?.symbol || '-',
+          supply: totalSupply.toLocaleString(),
+        },
+      };
+    },
   );
 
-  const tokenDatas =
-    data &&
-    data.length > 0 &&
-    data.map(
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      (token: any, index: number) => {
-        const totalSupply = token?.totalSupply / decimalToExponential(token.decimal);
-        const linkTokenExplorer = `${EXPLORER_URL}/token/${token?.address}`;
+  const onLoadMoreTokens = () => {
+    if (isFetching || tokensList.length % LIMIT_PAGE !== 0) return;
+    const page = Math.floor(tokensList.length / LIMIT_PAGE) + 1;
+    fetchTokensOwned(page, true);
+  };
 
-        return {
-          id: `token-${token?.address}}`,
-          render: {
-            number: index + 1,
-            name: (
-              <a href={linkTokenExplorer} rel="rel=”noopener noreferrer”" target="_blank">
-                {token?.name || '-'}
-              </a>
-            ),
+  const debounceLoadMore = debounce(onLoadMoreTokens, 300);
 
-            symbol: token?.symbol || '-',
-            supply: totalSupply.toLocaleString(),
-          },
-        };
-      },
-    );
+  useEffect(() => {
+    if (user && user.walletAddress) fetchTokensOwned();
+  }, [user]);
 
-  if (!data || data.length === 0 || !profileWallet) {
+  if (!tokensList || tokensList.length === 0 || !profileWallet) {
     return <Empty />;
   }
 
   return (
     <StyledTokenProfile>
-      {isLoading ? (
+      {isFetching ? (
         <div className="loading">
           <Spinner animation="border" variant="primary" />
         </div>
       ) : (
-        <Table tableHead={TABLE_HEADINGS} data={tokenDatas} className={'token-table'} />
+        <InfiniteScroll
+          className="transactions"
+          dataLength={tokensList?.length || 0}
+          hasMore={true}
+          loader={
+            isFetching && (
+              <div className="loading">
+                <Spinner animation="border" variant="primary" />
+              </div>
+            )
+          }
+          next={debounceLoadMore}
+        >
+          <Table tableHead={TABLE_HEADINGS} data={tokenDatas} className={'token-table'} />
+        </InfiniteScroll>
       )}
     </StyledTokenProfile>
   );
