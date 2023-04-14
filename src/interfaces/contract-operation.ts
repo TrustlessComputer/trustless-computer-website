7b2a0a701273f69a54b1e46dbd59d897073767d1
archaon <archaon@generative.xyz>
2023-04-14 09:52:53 +0700

    feat: update transaction ui

--- src/interfaces/contract-operation.ts
+++ src/interfaces/contract-operation.ts
@@ -1,10 +1,10 @@
 import { TransactionResponse } from '@ethersproject/abstract-provider';
 
 export enum DAppType {
-  ERC721 = 'ERC721', // NFTs
-  ERC20 = 'ERC20', // Tokens
-  BFS = 'BFS', // Artifactx
-  BNS = 'BNS', // Name
+  ERC721 = 'NFT', // NFTs
+  ERC20 = 'Token', // Tokens
+  BFS = 'Artifact', // Artifactx
+  BNS = 'Name', // Name
 }
 
 export type ContractOperationHook<P, R> = (arg?: any) => {
