460e4274c8df09cd025b05f1f5a0c1646090084c
archaon <archaon@generative.xyz>
2023-04-20 10:17:05 +0700

    feat: add photo tabs

---
+++ src/modules/Dapps/Photos/index.tsx
@@ -0,0 +1,58 @@
+import IconSVG from '@/components/IconSVG';
+import Text from '@/components/Text';
+import { CDN_URL } from '@/configs';
+import px2rem from '@/utils/px2rem';
+import { useState } from 'react';
+import { Tab, Tabs } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const StyledPhotos = styled.div`
+  margin-top: ${px2rem(48)};
+`;
+
+enum PhotoTabs {
+  ALL = 'all',
+  ALBUM = 'album',
+  IMAGE = 'image',
+}
+
+const Photos = () => {
+  const [activeTab, setActiveTab] = useState<string>(PhotoTabs.ALL);
+
+  const renderTabItem = (icon: string, text: string, total?: number) => {
+    return (
+      <div className="tab-item">
+        <div className="tab-icon">
+          <IconSVG maxWidth="32" maxHeight="32" src={''} url={`${icon}`} color="white" type="stroke"></IconSVG>
+        </div>
+        <Text className="tab-text" size="medium" fontWeight="medium">
+          {text} {total && total > 0 && `(${total})`}
+        </Text>
+      </div>
+    );
+  };
+
+  return (
+    <StyledPhotos>
+      <p>Photo Upload Component</p>
+      <Tabs
+        mountOnEnter
+        defaultActiveKey={activeTab}
+        id="uncontrolled-tab"
+        onSelect={key => setActiveTab(key || PhotoTabs.ALL)}
+      >
+        <Tab eventKey={PhotoTabs.ALL} title={renderTabItem(`${CDN_URL}/icons/ic-tab-nfts.svg`, 'NFTs')}>
+          {/* <Collections /> */}
+        </Tab>
+        <Tab eventKey={PhotoTabs.ALBUM} title={renderTabItem(`${CDN_URL}/icons/ic-tab-token.svg`, 'Tokens')}>
+          {/* <Tokens /> */}
+        </Tab>
+        <Tab eventKey={PhotoTabs.IMAGE} title={renderTabItem(`${CDN_URL}/icons/ic-tab-artifact.svg`, 'Artifacts')}>
+          {/* <Artifacts /> */}
+        </Tab>
+      </Tabs>
+    </StyledPhotos>
+  );
+};
+
+export default Photos;
