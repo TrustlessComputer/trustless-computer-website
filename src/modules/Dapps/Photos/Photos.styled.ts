4bd1780f0fee72dee102d1ed9d1475344948497a
archaon <archaon@generative.xyz>
2023-04-20 10:21:31 +0700

    chore: reorganize

--- src/modules/Dapps/Photos/Photos.styled.ts
+++ src/modules/Dapps/Photos/Photos.styled.ts
@@ -1,7 +1,7 @@
 import px2rem from '@/utils/px2rem';
 import styled from 'styled-components';
 
-export const Wrapper = styled.div`
+export const StyledPhotos = styled.div`
   padding-top: ${px2rem(60)};
   padding-bottom: ${px2rem(60)};
 `;
