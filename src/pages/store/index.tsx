f4ff690edac2b950634e2d33874539a5aaaef80e
hugrid2712 <2712@trustless.computer>
2023-05-29 13:54:56 +0700

    fix: update link get TC

--- src/pages/store/index.tsx
+++ src/pages/store/index.tsx
@@ -105,7 +105,7 @@ const DappsStorePage = () => {
         on Bitcoin: NFTs, DeFi, Payments, Gaming, DAOs, file storage, and many other dapps uniquely on Bitcoin.
       </p>
       <div className="header-actions">
-        <ButtonLink href="https://trustlessfaucet.io" target="_blank" className="header-actions-btn">
+        <ButtonLink href="https://tcgasstation.com/" target="_blank" className="header-actions-btn">
           <p className="button-link-text">Get TC</p>
         </ButtonLink>
         <div onClick={() => setShowModal(true)} className="submitBtn header-actions-btn">
